ea6a7832ae8ce8b78cbf35b98ba76bd73ff179b9
Vihaan Shah <vihaan.shah@uconn.edu>
2024-06-04 10:52:27 -0400

    Generate launch and config files for the moveit package using moveit setup assistant

---
+++ crm_moveit_config/launch/ur_moveit.launch.py
@@ -0,0 +1,44 @@
+from moveit_configs_utils import MoveItConfigsBuilder
+from launch import LaunchDescription
+from launch.actions import (
+    DeclareLaunchArgument,
+    IncludeLaunchDescription,
+)
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+
+from srdfdom.srdf import SRDF
+
+from moveit_configs_utils.launch_utils import (
+    add_debuggable_node,
+    DeclareBooleanLaunchArg,
+)
+
+def generate_launch(moveit_config):
+    ld = LaunchDescription()
+
+    ld.add_action(
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                str(moveit_config.package_path / "launch/move_group.launch.py")
+            ),
+        )
+    )
+
+    ld.add_action(
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                str(moveit_config.package_path / "launch/moveit_rviz.launch.py")
+            ),
+        )
+    )
+
+    return ld    
+
+def generate_launch_description():
+    moveit_config = MoveItConfigsBuilder("crm_robot", package_name="crm_moveit_config").to_moveit_configs()
+    return generate_launch(moveit_config)
