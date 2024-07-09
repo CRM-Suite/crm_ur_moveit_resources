2d2f6c9fbd5b0ef7e183f7dddd5bb92343ee0021
Vihaan Shah <vihaan.shah@uconn.edu>
2024-07-09 13:07:53 -0400

    Add initial_position to control launch

--- crm_single_robot_cell/crm_control/launch/rsp.launch.py
+++ crm_single_robot_cell/crm_control/launch/rsp.launch.py
@@ -1,34 +1,3 @@
-# Copyright (c) 2024 FZI Forschungszentrum Informatik
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#
-#    * Redistributions of source code must retain the above copyright
-#      notice, this list of conditions and the following disclaimer.
-#
-#    * Redistributions in binary form must reproduce the above copyright
-#      notice, this list of conditions and the following disclaimer in the
-#      documentation and/or other materials provided with the distribution.
-#
-#    * Neither the name of the {copyright_holder} nor the names of its
-#      contributors may be used to endorse or promote products derived from
-#      this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-
-#
-# Author: Felix Exner
-
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import (Command, FindExecutable, LaunchConfiguration,
@@ -47,7 +16,8 @@ def generate_launch_description():
     headless_mode = LaunchConfiguration("headless_mode")
 
     kinematics_parameters_file = LaunchConfiguration("kinematics_parameters_file")
-
+    initial_positions_file = LaunchConfiguration("initial_positions_file")
+    
     # Load description with necessary parameters
     robot_description_content = Command(
         [
@@ -78,6 +48,9 @@ def generate_launch_description():
             " ",
             "headless_mode:=",
             headless_mode,
+            " ",
+            "initial_positions_file:=",
+            initial_positions_file,
         ]
     )
     robot_description = {"robot_description": robot_description_content}
@@ -118,6 +91,19 @@ def generate_launch_description():
                 ]
             ),
             description="The calibration configuration of the actual robot used.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "initial_positions_file",
+            default_value=PathJoinSubstitution(
+                [
+                    FindPackageShare("crm_description"),
+                    "config",
+                    "initial_positions.yaml",
+                ]
+            ),
+            description="The path to the file with initial positions.",
         )
     )
     declared_arguments.append(
